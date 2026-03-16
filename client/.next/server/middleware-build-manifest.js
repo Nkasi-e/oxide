d8069c57a022ef10f474ecda259fc2425bf513fb
nkasi-e <nkasijnr@gmail.com>
2026-03-16 23:18:35 +0100

    delete .next file

--- client/.next/server/middleware-build-manifest.js
+++ client/.next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/repo/[owner]/[repo]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/repo/[owner]/[repo].js"
     ]
   },
   "ampFirstPages": []
